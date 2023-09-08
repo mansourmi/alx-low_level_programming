f9d521ff9a46c26f05371315b673bdc2879291b4
mansourmi <mansourmubarak35@gmail.com>
2023-09-08 01:57:57 +0000

    W

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,19 +3,17 @@
 /**
  *main - Entry point
  *
- *Description: using sizeof to print the size of various types.
- *
  *Return: Always 0 (success)
 */
 
 
 int main(void)
 {
-	ptintf("size of a char: %lu byte(s)\n", sizeof(char));
-	ptintf("size of an int: %lu byte(s)\n", sizeof(int));
-	ptintf("size of a long int: %lu byte(s)\n", sizeof(long int));
-	ptintf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	ptintf("size of a float: %lu byte(s)\n", sizeof(float));
+	ptintf("Size of a char: %d byte(s)\n", sizeof(char));
+	ptintf("Size of an int: %d byte(s)\n", sizeof(int));
+	ptintf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	ptintf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	ptintf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 
 }
