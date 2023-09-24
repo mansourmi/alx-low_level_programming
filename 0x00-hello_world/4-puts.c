2846ca9ff0a4c5004908ee35cf1b31c20585c9b6
mansourmi <mansourmubarak35@gmail.com>
2023-09-24 12:07:58 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Descriprtion - "Programming is like building a multilingual puzzle
+ *
+ * Return - Always 0 (success)
+*/
 int main(void)
 {
 
-	puts("Programming is like building a mulillingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
+
 	return (0);
 }
