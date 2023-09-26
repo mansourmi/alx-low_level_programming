b7b8e77f334947c91c864f57004dcf4b044cc98f
mansourmi <mansourmubarak35@gmail.com>
2023-09-26 16:55:23 +0000

    kik

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: a program print the lowercase alphabet
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
