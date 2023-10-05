0cc691b14321dce6e5f8536b0304a6a426f5ce7a
mansourmi <mansourmubarak35@gmail.com>
2023-10-05 18:48:24 +0000

    malloc pro

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -38,7 +38,7 @@ char *str_concat(char *s1, char *s2)
 	if (m == 0)
 		return (0);
 
-	for (i = 0; <= size1 + size2; i++)
+	for (i = 0; i <= size1 + size2; i++)
 	{
 		if (i < size1)
 			m[i] = s1[i];
