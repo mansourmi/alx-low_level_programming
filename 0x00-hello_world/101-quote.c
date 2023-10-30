00854d6011544d8daf93a0fe105d01c1865dee77
mansourmi <mansourmubarak35@gmail.com>
2023-10-30 21:13:51 +0000

    hello world advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,9 @@
 **/
 int main(void)
 {
-	
 
-	puts("and that piece of art is useful\"" - Dora Korpar, 2015-10-19);
+
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 
 	return (1);
 }
