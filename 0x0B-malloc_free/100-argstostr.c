ac2eb973d5cc25c9548651959c67f3bbd8140b82
mansourmi <mansourmubarak35@gmail.com>
2023-10-30 23:13:19 +0000

    malloc project

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -34,7 +34,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 
 	for (; i < ac; i++, nc++)
-		nc += _strlen(av[i];
+		nc += _strlen(av[i]);
 
 	s = malloc(sizeof(char) * nc + 1);
 	if (s == 0)
