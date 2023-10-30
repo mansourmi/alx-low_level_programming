3826ddcfe41363a22dc40edc4ec9a0630fb8099b
mansourmi <mansourmubarak35@gmail.com>
2023-10-30 21:04:49 +0000

    advnced project

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: write a quote
+ *
+ * Return: 1 succeed
+**/
+int main(void)
+{
+	
+
+	puts("and that piece of art is useful\"" - Dora Korpar, 2015-10-19);
+
+	return (1);
+}
