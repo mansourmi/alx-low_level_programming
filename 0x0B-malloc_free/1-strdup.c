405b7078145b6ff5735102227ccfcdd8dc521533
mansourmi <mansourmubarak35@gmail.com>
2023-10-30 21:50:07 +0000

    malloc project

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *_strdup - return pointer to new allocated space
+ *
+ * @str: string
+ *
+ * Return: 0 success
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *m;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[size] != '\0'; size++)
+	;
+
+	m = malloc(size * sizeof(*str) + 1);
+
+	if (m == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (; i < size; i++)
+			m[i] = str[i];
+	}
+	return (m);
+}
