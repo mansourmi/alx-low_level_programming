6dffe0e397c4615e65621dd33613762fd8dd9e63
mansourmi <mansourmubarak35@gmail.com>
2023-11-03 00:35:38 +0000

    more malloc free pro

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
 		;
 
-	for (s2_length = 0; s2[s2_length} != '\0'; s2_length++)
+	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
 		;
 	/*memory reservation for case 1 & 2*/
 	str = malloc(s1_length + n + 1);
@@ -35,12 +35,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	/* copy first string into str*/
 	for (i = 0; s1[i] != '\0'; i++)
-		str[i] = s[i];
+		str[i] = s1[i];
 	/*copy second string into str*/
 	for (j = 0; j < n; j++)
 	{
 		str[i] = s2[j];
-		i++
+		i++;
 	}
 
 	str[i] = '\0';
