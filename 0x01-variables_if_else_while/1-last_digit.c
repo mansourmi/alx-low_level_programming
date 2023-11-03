3a7d6440903b0c3a264c0cffaa85e5146c9577bf
mansourmi <mansourmubarak35@gmail.com>
2023-11-03 18:14:37 +0000

    if project

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, digit);
 	}
-	if ((digit < 6) != 0)
+	if (digit < 6 && != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	}
